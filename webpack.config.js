d5d3a44a7ccc9191ad5ba36d1c7ebe40f943a05b
Vladimir U <guru81@mail.ru>
2023-07-17 14:31:17 +0300

    не создается dist

--- webpack.config.js
+++ webpack.config.js
@@ -1,23 +1,25 @@
-const path = require('path')
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-  entry: './src/index.js',
-  module: {
-    rules: [
-      { test: /\.css$/, use: [ 'style-loader', 'css-loader' ] },
-      {
-        test: /\.(png|svg|jpg|jpeg|gif)$/i,
-        type: "asset/resource",
-      },
-      {
-        test: /\.(woff|woff2|eot|ttf|otf)$/i,
-        type: "asset/resource",
-      },
-    ]
-  },
-  output: {
-    path: path.resolve(__dirname, 'dist'),
-    filename: 'bundle.js',
-    clean: true,
-  }
-}
+    entry: './src/index.js',
+    module: {
+        rules: [
+            { test: /\.css$/, use: ['style-loader', 'css-loader'] },
+            {
+                test: /\.(png|svg|jpg|jpeg|gif)$/i,
+                type: 'asset/resource',
+            },
+            {
+                test: /\.(woff|woff2|eot|ttf|otf)$/i,
+                type: 'asset/resource',
+            },
+        ],
+    },
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'bundle.js',
+        clean: true,
+    },
+    plugins: [new HtmlWebpackPlugin()],
+};
